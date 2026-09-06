d4a483e31cb1cac5e078135a603a8e9c5fc7b515
DELL <harshnielit@gmail.com>
2026-09-06 23:15:54 +0530

    College project of banking

--- src/main/java/com/securebank/controller/BankController.java
+++ src/main/java/com/securebank/controller/BankController.java
@@ -1,9 +1,13 @@
 package com.securebank.controller;
+import com.securebank.pojo.RegisterForm;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 @Controller
 public class BankController {
 
@@ -16,10 +20,19 @@ public class BankController {
 
  @GetMapping("/register") public String register(HttpServletRequest req, Model m){
   String requestURI=req.getRequestURI();
+  m.addAttribute("sita",new RegisterForm());
   System.out.println(requestURI);
   m.addAttribute("mycurrentpage",requestURI);
   return "register";
  }
+
+ @GetMapping("/loginform")
+ public String registerForm(@ModelAttribute RegisterForm ram, Model m){
+  System.out.println(ram);
+  System.out.println("hi,ram");
+  return "register";
+ }
+
  @GetMapping("/dashboard") public String dashboard(HttpServletRequest req, Model m){
   String requestURI=req.getRequestURI();
   System.out.println(requestURI);
